4ddec7cae1908de615fd3ff2cdf69f774a80562d
Artem Nikolenko <kopo4e.web@gmail.com>
2020-10-26 22:10:35 +0300

    Upd: lesson 2

--- lesson_2/3.rb
+++ lesson_2/3.rb
@@ -3,7 +3,6 @@
 =end
 
 array = [1, 1]
-i = 1
 
 loop do
   fibonachi_num = array[-1] + array[-2]
@@ -11,7 +10,6 @@ loop do
   break if fibonachi_num > 100
 
   array << fibonachi_num
-  i += 1
 end
 
 puts array.inspect
