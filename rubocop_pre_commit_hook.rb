48bb4545a6381afd8283efb2374be78adb19dc3f
Artem Nikolenko <kopo4e.web@gmail.com>
2020-11-16 23:52:19 +0300

    rubocop pre commit hook

---
+++ rubocop_pre_commit_hook.rb
@@ -0,0 +1,32 @@
+# rubocop:disable Style/FrozenStringLiteralComment, Layout/LeadingCommentSpace
+#!/usr/bin/env ruby
+# rubocop:enable Style/FrozenStringLiteralComment, Layout/LeadingCommentSpace
+
+# rubocop:disable Style/StringLiterals
+ADDED_OR_MODIFIED = /^\s*(A|AM|M)/.freeze
+
+changed_files = `git status --porcelain`.split(/\n/)
+unstaged_files = `git ls-files -m`.split(/\n/)
+
+changed_files = changed_files.select { |f| f =~ ADDED_OR_MODIFIED }
+changed_files = changed_files.map { |f| f.split(" ")[1] }
+
+changed_files -= (unstaged_files - changed_files)
+
+changed_files = changed_files.select { |file_name| File.extname(file_name) == ".rb" }
+changed_files = changed_files.join(" ")
+
+exit(0) if changed_files.empty?
+
+success = system(%(
+  rubocop #{changed_files}
+))
+
+# STDIN.reopen('/dev/tty')
+$stdin.reopen('/dev/tty')
+
+if success == false
+  puts "Would you like to continue press 'any key' or 'n/N' to halt? "
+  exit(1) if %w[N n].include?(gets.chomp)
+end
+# rubocop:enable Style/StringLiterals
